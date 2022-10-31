e7d59a4ddc9d102bc3d6fec99e024c43636970bd
jinwook song <wlsdnr129@naver.com>
2022-10-31 22:06:28 +0900

    style: chakra ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,9 @@
-export const App = () => <div></div>;
+import { Text } from '@chakra-ui/react';
+
+export const App = () => (
+  <div>
+    <Text color={'cyan.600'} fontWeight={'semibold'} fontSize={'2xl'}>
+      Chakra UI
+    </Text>
+  </div>
+);
